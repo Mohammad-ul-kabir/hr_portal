41b35345a363e548239dce8f6a8dc2bfe377815d
Mohammad-ul-kabir <mohammadulkabirtushar@gmail.com>
2025-12-30 07:09:42 +0000

    register jsx delete

--- src/pages/EmployeeProfile.jsx
+++ src/pages/EmployeeProfile.jsx
@@ -14,7 +14,7 @@ function EmployeeProfile() {
   }
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50">
+    <div className="min-h-screen flex flex-col items-center mt-6 bg-gray-50">
       <h1 className="text-3xl font-bold mb-4 text-blue-600">
         Employee Profile
       </h1>
